f2a7b0abc60849319d267035d134c71e9f2d46e8
ld213344 <2310596366@qq.com>
2016-10-04 13:45:24 +0800

    Add files via upload

---
+++ shkj/js/lib.js
@@ -0,0 +1,25 @@
+﻿$(function(){
+	//动画效果弹出菜单
+	$(".MENUS li").hover(function(){
+		$(this).addClass("ok").find("dl").stop(true,true).slideUp(0).slideDown(300);
+	},function(){
+		$(".ok").find("dl").stop(true,true).slideUp(200);
+		$(this).removeClass("ok");
+	});
+	$(".menuBtn").append('<b></b><b></b><b></b>');
+	$(".menuBtn").click(function() {
+		$(this).toggleClass("open");
+		$(".menu").toggleClass ("on");
+	});
+});
+$(function(){
+	// 选项卡 鼠标点击
+	$(".TAB_CLICK li").click(function(){
+		var tab=$(this).parent(".TAB_CLICK");
+		var con=tab.attr("id");
+		var on=tab.find("li").index(this);
+		$(this).addClass('hover').siblings(tab.find("li")).removeClass('hover');
+		$(con).eq(on).show().siblings(con).hide();
+	});
+});
+
